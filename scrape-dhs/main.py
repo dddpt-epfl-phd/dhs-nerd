80b5f3b793e6247b8b75d3262c31f029e6a52e70
dddpt <didier.dupertuis@epfl.ch>
2021-10-14 10:21:53 +0200

    setup already visited ids for german

--- scrape-dhs/main.py
+++ scrape-dhs/main.py
@@ -1,25 +1,39 @@
+# %%
 
+import re
 
 from dhs_scraper import DhsArticle
 
+
+language="de"
+output_file = f"dhs_all_articles_{language}.jsonl"
+
+
 # %%
 
-buffer_size = 100
-buffer = [None]*100
+
+id_regex = re.compile(r'^.+?"id": "(\d+)"')
+with open(output_file, "r") as dhs_all_json_file:
+    already_visited_ids = set(id_regex.search(line).group(1) for line in dhs_all_json_file if len(line)>0)
+
+# %%
+
+buffer_size = 10
+buffer = [None]*10
 
 # scrape args
 max_nb_articles_per_letter = None
 parse_articles = True
-language="de"
 skip_duplicates=True
 
-with open(f"dhs_all_articles_{language}.json", "a") as dhs_all_json_file:
+with open(output_file, "a") as dhs_all_json_file:
     for i, a in enumerate(DhsArticle.scrape_all_articles(
         language=language,
         max_nb_articles_per_letter=max_nb_articles_per_letter,
         parse_articles = parse_articles,
         force_language = language,
-        skip_duplicates = skip_duplicates)):
+        skip_duplicates = skip_duplicates,
+        already_visited_ids = already_visited_ids)):
         if i!=0 and i%buffer_size==0:
             print(f"BUFFER OUT i: {i} \nbuffer[0]:{buffer[0]}\nbuffer[99]:{buffer[99]}")
             dhs_all_json_file.write("\n".join(buffer)+"\n")
