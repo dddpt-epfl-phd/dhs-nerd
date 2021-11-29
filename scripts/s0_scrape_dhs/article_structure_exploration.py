6f50a77fd685c8c76698f2a8dbd6b67517e80d38
dddpt <didier.dupertuis@epfl.ch>
2021-11-29 18:03:06 +0100

    DhsA.parse_text_links() added!

--- scripts/s0_scrape_dhs/article_structure_exploration.py
+++ scripts/s0_scrape_dhs/article_structure_exploration.py
@@ -1,10 +1,9 @@
 # %%
 from functools import reduce
 from random import sample, seed
-import re
 
 from lxml import html
-import pandas as pd
+from lxml.etree import iselement
 from webbrowser import open as op
 
 import sys
@@ -43,7 +42,70 @@ for tre in to_remove_elements:
 
 elements = vaud._pagetree.cssselect(".hls-article-text-unit p, h1, h2, .hls-article-text-unit h3, .hls-article-text-unit h4")
 text = reduce(lambda s,el: s+el.text_content()+"\n\n", elements, "")[0:-2]
-        
 
+# %%
+links = [e.cssselect("a") for e in elements]
+links_flat = [a for ases in links for a in ases]
+
+
+# %%
+
+for t in elements[3].iter():
+    print(f"LEVEL 0: <{t.tag}> {t.text_content()}")
+    for t1 in t:
+        print(f"- <{t1.tag}> {t1.text_content()}")
+
+# %%
+
+for node in elements[3].xpath("child::node()"):
+    print(f"NODE: {node} iselement: {iselement(node)}")
+
+# %%
+for t in elements[3].iterlinks():
+    print(t)
+# %%
+for t in elements[3].iterdescendants("*"):
+    print(f"TEXT:\n{t}")
+# %%
+for t in elements[3].itertext():
+    print(f"TEXT:\n{t}")
+
+# %%
+
+def lxml_depth_first_iterator(element, iteration_criterion):
+    """iterate depth-first over an lxml element, yielding elements according to iteration_criterion()
+
+    Considers both text and Element nodes.
+
+    yields:
+    - all descendants d of element for which iteration_criterion(d) returns True
+    """
+    for node in element.xpath("child::node()"):
+        print(f"lxml_dpi() node: {node}, iteration_criterion(node): {iteration_criterion(node)}, iselement(node): {iselement(node)}")
+        if iteration_criterion(node):
+            yield node
+        elif iselement(node):
+            for n in lxml_depth_first_iterator(node, iteration_criterion):
+                yield n
+
+def is_text_or_link(node):
+    if (not iselement(node)) or (node.tag=="a"):
+        return True
+    return False
+
+text_and_links = [tl for tl in lxml_depth_first_iterator(elements[3], is_text_or_link)]
+text_and_links
+
+# %% Check results:
+
+vaud.parse_text_links()
+# -> good!
+
+# %%
+
+
+othon = DhsArticle(url="https://hls-dhs-dss.ch/fr/articles/017791/2008-01-21/")
+othon.parse_text_links()
+# -> good!
 
 # %%
