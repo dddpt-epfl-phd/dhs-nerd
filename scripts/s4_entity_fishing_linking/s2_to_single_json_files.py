f2cf9d36a052694294800c0a8637de3d91b524a1
dddpt <didier.dupertuis@epfl.ch>
2022-01-04 14:59:42 +0100

    lng command line arg

--- scripts/s4_entity_fishing_linking/s2_to_single_json_files.py
+++ scripts/s4_entity_fishing_linking/s2_to_single_json_files.py
@@ -15,28 +15,40 @@ from data_file_paths import S4_JSONL_ALL_ARTICLES_LINKED_FILE, S5_WEBSITE_SINGLE
 """
 This scripts load all the DhsArticles in S0_JSONL_ALL_ARTICLES_FILE and writes them to single json files
 """
+max_nb_articles=19000
+
 
 # LOAD LINKED ARTICLES AND STREAMING THEM TO INDIVIDUAL JSON
-lng="de"
-max_nb_articles=10000
+default_language="it"
+entity_fishing_timeout = None
+
+language = default_language
+possible_languages = ["fr", "de", "it"]
+if len(sys.argv)>1:
+    if sys.argv[1] in possible_languages:
+        language = sys.argv[1]
+    else:
+        raise Exception(f"s0_scrape.py: unrecognized language argument from sys.argv[1]: '{sys.argv[1]}'. Must be one of {possible_languages}")
 
 
-jsonl_linked_articles_file = localize(S4_JSONL_ALL_ARTICLES_LINKED_FILE, lng)
-json_folder = localize(S5_WEBSITE_SINGLE_ARTICLES_LOCALIZED_FOLDER, lng)
+
+
+jsonl_linked_articles_file = localize(S4_JSONL_ALL_ARTICLES_LINKED_FILE, language)
+json_folder = localize(S5_WEBSITE_SINGLE_ARTICLES_LOCALIZED_FOLDER, language)
 
 articles_ids_titles = list()
 for i,a in enumerate(DhsArticle.load_articles_from_jsonl(
-            localize(S4_JSONL_ALL_ARTICLES_LINKED_FILE,lng),
+            localize(S4_JSONL_ALL_ARTICLES_LINKED_FILE,language),
             indices_to_keep=[i for i in range(max_nb_articles)]
         )):
     with open(path.join(json_folder, f'{a.id}.json'), 'w') as f:
-        print(f"{i} writing {lng} json for {a.title}")
+        print(f"{i} writing {language} json for {a.title}")
         json.dump(a.to_json(as_dict=True), f)
     articles_ids_titles.append((a.id, a.search_result_name))
 
-index_json = localize(S5_WEBSITE_SINGLE_ARTICLES_LOCALIZED_FOLDER, "indices/"+lng+".json")
+index_json = localize(S5_WEBSITE_SINGLE_ARTICLES_LOCALIZED_FOLDER, "indices/"+language+".json")
 with open(index_json, 'w') as f:
-    print(f"writing index json for {lng}")
+    print(f"writing index json for {language}")
     json.dump(articles_ids_titles, f)
 
 
