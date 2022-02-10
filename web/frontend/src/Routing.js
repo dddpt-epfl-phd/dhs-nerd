01f544f9a449ae351a0fc1a1f8de78b9619aebd3
dddpt <didier.dupertuis@epfl.ch>
2022-02-10 10:46:06 +0100

    BUG FIX HASHROUTER BASENAME IN ROUTES, NOT ROUTER

--- web/frontend/src/Routing.js
+++ web/frontend/src/Routing.js
@@ -16,14 +16,14 @@ console.log("ROOOOOOUUUUTING, basename is: ", process.env.PUBLIC_URL)
 const Route404 = ({tadu="DEFAULT"})=><div>THIS IS A {tadu} ROUTE</div>
 export default function Routing() {
   console.log("doin' the routin', basename is: ", process.env.PUBLIC_URL)
-  const location = useLocation();
-  console.log("ROOUTING loaction: ",location);
+  //const location = useLocation();
+  console.log("ROOUTING window.location.hash: ", window.location.hash);
   return (
     <>
-    HOLLA HOWDY
+    HOLLA HOWDYYAB
     <Route404 tadu="TEST"/>
-    <Router basename={process.env.PUBLIC_URL}>
-      <Routes>
+    <Router>
+      <Routes basename={process.env.PUBLIC_URL}>
           <Route path="/:language/articles/:dhsId" element={<DhsArticle />}/>
           <Route path="/:language" element={<ArticlesList />}/>
           <Route path="/:language/articles" element={<ArticlesList />}/>
