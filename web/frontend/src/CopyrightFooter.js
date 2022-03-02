c4f2dd39cc860a5ffb966e6a3e0e509852e0fc17
dddpt <didier.dupertuis@epfl.ch>
2022-03-02 17:29:29 +0100

    bug fix react child as array

--- web/frontend/src/CopyrightFooter.js
+++ web/frontend/src/CopyrightFooter.js
@@ -3,7 +3,7 @@ import React from "react";
 export const CopyrightFooter = (originalPageLink="")=> {
     return (
         <div id="copyright-footer">
-            The content of all HDS articles presented on this website are the work of their respective author from the original HDS. {originalPageLink} <br/>
+            The content of all HDS articles presented on this website are the work of their respective author from the original HDS. {[originalPageLink]} <br/>
             This work is available under the <a href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank">Creative Common Attribution-ShareAlike 4.0 International License (CC BY-SA 4.0) </a>
         </div>
     )
