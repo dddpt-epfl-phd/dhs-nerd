9f15aedd6067d982f84a43ab9172a981f3dd0ea3
dddpt <didier.dupertuis@epfl.ch>
2022-03-07 10:29:14 +0100

    improved copyright footer

--- web/frontend/src/CopyrightFooter.js
+++ web/frontend/src/CopyrightFooter.js
@@ -4,8 +4,8 @@ export const CopyrightFooter = ({children=[]})=> {
     console.log("CopyrightFooter children: ", children)
     return (
         <div id="copyright-footer">
-            The content of all HDS articles presented on this website are the work of their respective author from the original HDS. {children} <br/>
-            This work is available under the <a href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank">Creative Common Attribution-ShareAlike 4.0 International License (CC BY-SA 4.0) </a>
+            The content of all HDS articles presented on this website are the work of their respective author from the original HDS. {children}
+            This work is available under the Creative Common Attribution-ShareAlike 4.0 International License (<a href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank">CC BY-SA 4.0</a>)
         </div>
     )
 }
--- web/frontend/src/DhsArticle.js
+++ web/frontend/src/DhsArticle.js
@@ -95,7 +95,7 @@ export function DhsArticleContent({
             {textBlocks}
             <hr/>
             <CopyrightFooter>
-                <RealDhsArticleLink dhsId={article.id}>Original article available here.</RealDhsArticleLink>
+                Original article available <RealDhsArticleLink dhsId={article.id}>here</RealDhsArticleLink>.
                 <span></span>
             </CopyrightFooter>
         </div>
