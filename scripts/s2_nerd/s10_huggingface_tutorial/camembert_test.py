a4ee31dc846614a57dff435b828a6943b990f1b8
dddpt <didier.dupertuis@epfl.ch>
2023-02-02 11:09:02 +0100

    hf + displacy working!

--- scripts/s2_nerd/s10_huggingface_tutorial/camembert_test.py
+++ scripts/s2_nerd/s10_huggingface_tutorial/camembert_test.py
@@ -1,5 +1,7 @@
 
 # %%
+import json
+
 from transformers import AutoTokenizer, AutoModelForTokenClassification, pipeline
 
 import torch
@@ -61,7 +63,22 @@ tokenizer = AutoTokenizer.from_pretrained(checkpoint)
 # max_length for camemBERT is 512 tokens
 inputs = tokenizer(raw_inputs, padding="max_length", truncation=True, return_tensors="pt")
 
+
 # %%
+#tokenzz = [tokenizer.decode(id, skip_special_tokens=True) for id in inputs.input_ids[0]]
+tokens_text = [tokenizer.decode(id, skip_special_tokens=True) for ids in inputs.input_ids for id in ids]
+tokens_text
+# %%
+tokens = [tokenizer.convert_ids_to_tokens(ids, skip_special_tokens=True) for ids in inputs.input_ids]
+tokens
+# %%
+tokens = tokenizer.convert_ids_to_tokens(inputs.input_ids[0], skip_special_tokens=True)
+# tokens[0].start
+# %%
+# ! important: decoded text doesn't include line returns!!
+decoded_text_from_tokens = tokenizer.convert_tokens_to_string(tokens)
+decoded_text_from_input_ids = tokenizer.decode(inputs.input_ids[0], skip_special_tokens=True)
+decoded_text_from_input_ids
 
 # %%
 
@@ -96,8 +113,105 @@ model
 camembert_ner = pipeline('ner', model=model, tokenizer=tokenizer, aggregation_strategy="simple")
 
 # %%
-camembert_ner(
-    sampled_articles_dtf.article.iloc[99].text_blocks[33][1]
-)
+text = sampled_articles_dtf.article.iloc[99].text_blocks[33][1]
 
+ner_results = camembert_ner(
+    text
+)
+ner_results
+ # %%
+for r in ner_results:
+    r["score"] = float(r["score"])
+
+# %%
+
+
+# %%
+def json_dump_huggingface_ner_results(text, ner_results, jsonfile="huggingface_ner_results.json"):
+    with open(jsonfile, "w") as nerf:
+        json.dump({"text":text, "ner_results":ner_results}, nerf, indent="\t", ensure_ascii=False)
+
+# %%
+json_dump_huggingface_ner_results(text, ner_results, jsonfile="huggingface_ner_results.json")
+# %%
+
+def tokens_ner_to_displacy_labels(tokens_texts, ner_results):
+    """
+    Algo
+    - loop over the tokens
+        - compute start + end of current_token
+        - compute current_ner_result based on those limits
+        - if current token in ner_result set up label
+        - if current_ner_result out, jump out of it.
+    """
+    ents = []
+    token_start = 0
+    token_end = 0
+    current_ner_result_index = 0
+    current_ner_result = ner_results[current_ner_result_index]
+    for token in tokens_texts:
+        token_start = token_end
+        token_end = token_start+len(token)
+        if token_start>= current_ner_result["start"] and token_end<= current_ner_result["end"]:
+            ents.append(current_ner_result["entity_group"])
+        elif token_start>= current_ner_result["start"] and token_end>= current_ner_result["end"]:
+            print(f"PROBLEM OVERLAPPING TOKENS token_start={token_start}, token_end={token_end}")
+            print(f'\tcurrent_ner_result["start"]={current_ner_result["start"]}, current_ner_result["end"]={current_ner_result["end"]}')
+        else:
+            ents.append("O")
+        if token_start >=current_ner_result["end"]:
+            current_ner_result_index +=1
+            current_ner_result = ner_results[current_ner_result_index]
+    return ents
+
+# %%
+
+ner_ents = tokens_ner_to_displacy_labels(tokens_text, ner_results)
+# %%
+ner_results
+
+# %%
+
+# HF+DISPLACY TEST
+
+from spacy.tokens import Doc
+from spacy.vocab import Vocab
+from spacy import displacy
+
+ents_mockup = ["O", "O", "O", "O", "I-LOC", "O", "O", "I-PER", "O", "I-LOC"]
+ds_tokens = [t for t in tokens_text if len(t)>0]
+ents = [ents_mockup[i %len(ents_mockup)] for i,t in enumerate(ds_tokens)]
+ents
+# %%
+
+doc = Doc(Vocab(strings=set(ds_tokens)),
+        words=ds_tokens,
+        # spaces=spaces,
+        ents=ents
+)
+displacy.render(doc, style="ent")
+
+# %%
+
+# overkill at the moment: simply pass text+identified entities
+if False:
+    def hf_tokens_to_json_tokens(tokens_str):
+        pass
+
+    # %%
+
+
+    def text_to_json_tokens(tokenizer, input_ids=None, text=None):
+        if input_ids is None and text is None:
+            raise("Error: text_to_json_tokens() expects at least one of input_ids or text to be not None")
+        if input_ids is None:
+            input_ids = tokenizer(text, padding="max_length", truncation=True, return_tensors="pt").input_ids[0]
+
+        #tokenzz = [tokenizer.decode(id, skip_special_tokens=True) for id in inputs.input_ids[0]]
+        tokens_text = [tokenizer.decode(id, skip_special_tokens=True) for ids in inputs.input_ids for id in ids]
+        tokens_text
+
+
+    def predictions_to_to_json_tokens():
+        pass 
 # %%
